60ec166f9a2e314eb73da7c9c07eb8d494fa2701
kevinkowa <kevin.kowalski01@utrgv.edu>
2018-03-12 03:24:05 -0500

    fixing ckeditor

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require rails-ujs
 //= require turbolinks
-//= require ckeditor/init
+//= require ckeditor/override`
+//= require ckeditor/init`
 //= require bootstrap-sprockets
 //= require_tree .
